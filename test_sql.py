c3aa4b7dbbe701197c5d17c0f4cc8eb0231c6012
Diyor Bobokalonov <diyor.b.h@gmail.com>
2024-09-03 22:38:21 +0400

    updated .gitignore

--- test_sql.py
+++
@@ -1,9 +0,0 @@
-from models.stargate_networks import StargateNetwork
-from data.config import DATABASE_NAME
-from models.database import Session
-
-session = Session()
-
-q = session.query(StargateNetwork)
-for i in q:
-    print(i)
